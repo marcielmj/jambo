52eb07716b4e3015c703723dfe421ec0ffde0307
Marciel Sousa <marcielmj@gmail.com>
2019-04-04 00:35:29 -0300

    Fix CORS headers

--- server/app/middlewares/cors.js
+++ server/app/middlewares/cors.js
@@ -3,5 +3,6 @@
 module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS')
   next()
 }
